3a5e1b81176dfd4ee5cdd5d211a3edf068ac5223
Carlton Brady <ctombrady@gmail.com>
2024-12-20 22:39:51 -0500

    day 4 part 2 solution

--- src/day4/mod.rs
+++ src/day4/mod.rs
@@ -9,20 +9,6 @@ pub fn solve() {
         process::exit(1);
     });
 
-    // let test_input = "\
-    //     MMMSXXMASM\n \
-    //     MSAMXMSMSA\n \
-    //     AMXSXMAAMM\n \
-    //     MSAMASMSMX\n \
-    //     XMASAMXAMM\n \
-    //     XXAMMXXAMA\n \
-    //     SMSMSASXSS\n \
-    //     SAXAMASAAA\n \
-    //     MAMMMXMMMM\n \
-    //     MXMXAXMASX\n";
-    //
-    // contents = test_input.to_string();
-
     // construct a 2D vec of the input
     let mut matrix: Vec<Vec<char>> = vec![];
 
@@ -32,6 +18,7 @@ pub fn solve() {
         };
     }
 
+    // Part 1
     // Use a 4 x 4 sliding window, store the start and end coordinates of
     // every XMAS found, then count the total unique XMAS's at the end
     let xmas: String = String::from("XMAS");
@@ -103,4 +90,32 @@ pub fn solve() {
         }
     }
     println!("XMAS count: {}", found.len());
+
+    // Part 2
+    // Use a 3 x 3 sliding window, count each MAS | SAM pair in the form of an X
+    let mas: String = String::from("MAS");
+    let mas_rev: String = mas.chars().rev().collect();
+    let mut mas_count = 0;
+
+    for i in 0..matrix.len() - 2 {
+        for j in 0..matrix[0].len() - 2 {
+            // Check diagonal top left to bottom right
+            let mut word1: String = String::new();
+            for k in 0..3 {
+                word1.push(matrix[i + k][j + k]);
+            }
+
+            // Check diagonal top right to bottom left
+            let mut word2: String = String::new();
+            for k in 0..3 {
+                word2.push(matrix[i + k][j + (2 - k)]);
+            }
+
+            if (word1 == mas || word1 == mas_rev) && (word2 == mas || word2 == mas_rev) {
+                mas_count += 1;
+            }
+        }
+    }
+
+    println!("X-MAS count: {}", mas_count);
 }
